2a7c410fdc6173a2abd4282fb8c6fa40051419ca
Vincent Truche <vince.truche@gmail.com>
2022-05-05 16:02:03 +0200

    Exo12 - Destructuration d'objet

---
+++ 12-VALIDER-LES-INPUTS-AVEC-JOI/demo-destructuring.js
@@ -0,0 +1,26 @@
+const result = {
+      value: "hey",
+      error: "ho",
+      blabla: "ha"
+}
+
+const {value, error, blabla} = result;
+
+console.log(value, error, blabla);
+
+const result2 = {
+      v1: "&",
+      v2: "1"
+}
+
+const {v1} = result2
+
+console.log(v1)
+
+const result3 = {
+      a: "AAAA",
+}
+
+const {a, enplus} = result3
+
+console.log(a, enplus);
