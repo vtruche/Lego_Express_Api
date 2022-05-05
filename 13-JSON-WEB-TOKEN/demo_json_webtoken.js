53b0a236072d496728227bd8d95e118f5bcfe22c
Vincent Truche <vince.truche@gmail.com>
2022-05-05 17:00:54 +0200

    Exo13 - Signer et Vérifier le Token

--- 13-JSON-WEB-TOKEN/demo_json_webtoken.js
+++ 13-JSON-WEB-TOKEN/demo_json_webtoken.js
@@ -1,24 +1,15 @@
 const jwt = require("jsonwebtoken");
+require('dotenv').config();
 
-const token = jwt.sign({userId: 12}, "MY_JWT_SECRET");
+
+const token = jwt.sign({userId: 12}, process.env.SECRET_JWT);
 console.log(token);
 
-const decoded = jwt.verify(token, "MY_JWT_SECRET");
+const decoded = jwt.verify(token, process.env.SECRET_JWT);
 console.log(decoded);
 
 try {
-    const notdecoded = jwt.verify(token, "PAS_LE_BON_SECRET");
-} catch(exc) {
-    console.log('PAS LA BONNE SIGNATURE');
+  const notdecoded = jwt.verify(token, "PAS_LE_BON_SECRET");
+}catch(exc){
+  console.log('PAS LA BONNE SIGNATURE');
 }
-
-// create bad token
-const token2 = jwt.sign({userId: 14}, "MY_JWT_SECRET");
-console.log(token2);
-
-const token_splitted = token.split(".");
-const token2_splitted = token2.split(".");
-
-const fakeToken = token2_splitted[0] + "." + token2_splitted[1] + "." + token_splitted[2];
-
-console.log(fakeToken);
