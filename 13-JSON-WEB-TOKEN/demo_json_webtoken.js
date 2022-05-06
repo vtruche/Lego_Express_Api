d5548ae1290797e7cc509e1a2969709ed7871522
Vincent Truche <vince.truche@gmail.com>
2022-05-06 10:09:03 +0200

    Exo13 : Protection pour ne pas exécuter le code sans Secret

--- 13-JSON-WEB-TOKEN/demo_json_webtoken.js
+++ 13-JSON-WEB-TOKEN/demo_json_webtoken.js
@@ -1,15 +1,38 @@
 const jwt = require("jsonwebtoken");
 require('dotenv').config();
 
+if (!process.env.SECRET_JWT) {
+  console.error('ERROR: Une variable d\'environnement SECRET_JWT doit existée');
+  process.exit(1);
+}
+
 
 const token = jwt.sign({userId: 12}, process.env.SECRET_JWT);
-console.log(token);
+console.log("token JWT", token);
 
 const decoded = jwt.verify(token, process.env.SECRET_JWT);
-console.log(decoded);
+console.log("token JWT décodé", decoded);
 
 try {
   const notdecoded = jwt.verify(token, "PAS_LE_BON_SECRET");
 }catch(exc){
-  console.log('PAS LA BONNE SIGNATURE');
+  console.log("échec de jwt.verify : PAS LA BONNE SIGNATURE");
+}
+
+// faketoken
+
+const token2 = jwt.sign({userId: 13}, process.env.SECRET_JWT);
+
+const header = token.split('.')[0];
+const payload = token.split('.')[1];
+const signature = token2.split('.')[2];
+
+const fakeToken = header +"."+ payload +"."+ signature
+console.log("FAKE TOKEN : ", fakeToken);
+
+
+try {
+  const failedVerification = jwt.verify(fakeToken, process.env.SECRET_JWT);
+}catch(exc){
+  console.log('echec de la vérification du Faux Token : PAS LA BONNE SIGNATURE');
 }
